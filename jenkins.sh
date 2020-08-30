9ebff19c2df2331f71b8fba1281e688ff8955458
Reem Als <46527514+Reemns97@users.noreply.github.com>
2020-08-31 00:45:59 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+	    k3d cluster create labs \
+	    -p 80:80@loadbalancer \
+	    -p 443:443@loadbalancer \
+	    -p 30000-32767:30000-32767@server[0] \
+	    -v /etc/machine-id:/etc/machine-id:ro \
+	    -v /var/log/journal:/var/log/journal:ro \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+	    --agents 3
 kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
 WAIT=90
 echo "Sleeping for $WAIT"
