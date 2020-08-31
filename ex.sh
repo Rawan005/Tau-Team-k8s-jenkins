d453a06d9fb1658d37dd23f63e629db938b948f0
Rawan005 <66908568+Rawan005@users.noreply.github.com>
2020-09-01 01:16:27 +0300

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,1 +1,5 @@
 kubectl get node --selector='!node-role.kubernetes.io/agent'
+
+k3d-tauteam-cluster-agent-0    Ready    <none>   7m6s   v1.18.6+k3s1
+k3d-tauteam-cluster-agent-1    Ready    <none>   7m6s   v1.18.6+k3s1
+k3d-tauteam-cluster-server-0
