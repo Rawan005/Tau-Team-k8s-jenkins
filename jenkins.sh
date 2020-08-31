53e1a5039dd9ac7e4a421a7ea5321690c02db45c
Rawan005 <66908568+Rawan005@users.noreply.github.com>
2020-09-01 01:24:38 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-	    k3d cluster create labs \
+	    k3d cluster create Tauteam-Cluster \
 	    -p 80:80@loadbalancer \
 	    -p 443:443@loadbalancer \
 	    -p 30000-32767:30000-32767@server[0] \
 	    -v /etc/machine-id:/etc/machine-id:ro \
 	    -v /var/log/journal:/var/log/journal:ro \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
-	    --agents 3
+	    --agents 2
 kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
+kubectl  label node k3d-tauteam-cluster-agent-0 type=worker-0
+kubectl  label node k3d-tauteam-cluster-agent-1 type=worker-0
+kubectl  label node k3d-tauteam-cluster-server-0 type=master
 WAIT=90
 echo "Sleeping for $WAIT"
 sleep $WAIT
